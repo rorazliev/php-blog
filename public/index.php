50acdc6d39fcfd686b91fdb51703b5dc0edd2907
Ruslan Orazliev <rorazliev@outlook.com>
2021-01-10 17:46:22 -0500

    Replace composer with spl_load_register

--- public/index.php
+++ public/index.php
@@ -1,6 +1,19 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+spl_autoload_register(function ($class) {
+  $exploded = explode('\\', $class);
+  $fileName = array_pop($exploded) . '.php';
+
+  $exploded = array_map(function ($directory) {
+    return strtolower($directory);
+  }, $exploded);
+
+  $imploded = implode('/', $exploded);
+
+  if (file_exists($path = __DIR__ . '/../' . $imploded . '/' . $fileName)) {
+    require_once $path;
+  }
+});
 
 use App\Routing\Router;
 
