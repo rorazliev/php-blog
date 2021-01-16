1ab92867d5c58e31102883e935876cf5633f34d3
Ruslan Orazliev <rorazliev@outlook.com>
2021-01-16 15:51:20 -0500

    Register new routes

--- public/index.php
+++ public/index.php
@@ -22,6 +22,9 @@ spl_autoload_register(function ($class) {
 use App\Routing\Router;
 
 $router = new Router;
+$router->get('/', 'HomeController::index');
+$router->get('/page/{id:\d+}', 'HomeController::index');
+$router->get('/about', 'HomeController::about');
 $router->get('/contact', 'ContactController::render');
 $router->post('/contact', 'ContactController::sendEmail');
 $router->dispatch();
