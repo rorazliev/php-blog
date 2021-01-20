c0d0ae68241312d3563b0b748252535695e7cc18
Ruslan Orazliev <rorazliev@outlook.com>
2021-01-19 21:08:20 -0500

    Add login template

---
+++ app/templates/admin/login.php
@@ -0,0 +1,17 @@
+<main class="main" role="main">
+  <div class="main-inner">
+    <form class="login-form" method="post">
+      <div class="form-group">
+        <label for="username">Username</label>
+        <input class="form-input" type="text" name="username">
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input class="form-input" type="password" name="password">
+      </div>
+      <div class="form-group">
+        <button class="login-button" type="submit">Log In</button>
+      </div>
+    </form>
+  </div>
+</main>
